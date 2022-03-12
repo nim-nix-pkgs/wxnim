b5d5c9f6b2783d6aca2686bf8ca95a522d5f72ec
hugosenari <hugosenari@gmail.com>
2022-03-12 02:11:06 -0300

    chore: re index wxnim

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Nim wrapper for wxWidgets. Also contains high-level genui macro'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."wxnim-master".url = "path:./master";
+  inputs."wxnim-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."wxnim-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
